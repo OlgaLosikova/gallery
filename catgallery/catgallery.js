94c09cde35844e661e2c5c51593ded23d62d06c3
ov.evdokimova <>
2023-10-15 19:30:55 +0300

    first commit

---
+++ catgallery/catgallery.js
@@ -0,0 +1,10 @@
+const changePicNext = () => {
+  const image = document.getElementById("image");
+  image.src =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/RedCat_8727.jpg/1200px-RedCat_8727.jpg";
+};
+const changePicPrevious = () => {
+  const image = document.getElementById("image");
+  image.src =
+    "https://www.thesprucepets.com/thmb/uQnGtOt9VQiML2oG2YzAmPErrHo=/5441x0/filters:no_upscale():strip_icc()/all-about-tabby-cats-552489-hero-a23a9118af8c477b914a0a1570d4f787.jpg";
+};
